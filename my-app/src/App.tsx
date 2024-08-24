30f8807f9f8b3e4d22bf0712eaeaa62fb5fc5ff6
Isanka Suresh <isankasuresh001@gmail.com>
2024-08-24 23:41:16 +0530

    implemented the newsApi

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,22 +2,29 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import axios from 'axios';
 import { sample } from './data/SampleData';
-import NewsCard from './component/NewsCard';;
+import NewsCard from './component/NewsCard';
 
 function App() {
 
-  //const [articles, setArticles] = useState<Article[]>([]);
+  const [articles, setArticles] = useState<article[]>([]);
   //const [persons, setPerson] = useState<person[]>([]);
 
-  // useEffect(() => {
-  //fetchNews().then((data) => setArticles(data))
-  //   {articles.length > 0 ? (
-  //   articles.map((article) =>
-  //     (console.log(article.author)))
-  // ):(
-  //   console.log("nothing")
-  // )};
-  //})
+  useEffect(() => {
+    fetchNews();
+    
+  },[])
+
+  const fetchNews = async () => {
+    try {
+      const response = await axios.get('https://newsapi.org/v2/top-headlines?sources=bbc-news,the-verge&apiKey=0353eca61c1a40bea318234fbacab6b8');
+      //&from=2024-08-23&to=2024-08-24&domains=engadget.com&pageSize=10&page=1
+      const data = response.data;
+      setArticles(data.articles); 
+      console.log(articles)
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
   // interface person {
   //   id: number;
@@ -25,44 +32,37 @@ function App() {
   // }
 
 
-  // interface Article {
-  //   source: {
-  //     id: string | null;
-  //     name: string;
-  //   };
-  //   author: string;
-  //   title: string;
-  //   description: string;
-  //   url: string;
-  //   urlToImage: string;
-  //   publishedAt: string;
-  //   content: string;
-  // }
+  interface article {
+    source: {
+      id: string | null;
+      name: string;
+    };
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+  }
 
-  // const fetchNews = async () => {
-  //   try {
-  //     const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=287e94250c0d40b5a4342f1e05e744c4');
-  //     //&from=2024-08-23&to=2024-08-24&domains=engadget.com&pageSize=10&page=1
-  //     const data = response.data;
-  //     console.log(response.status);
-  //     return data;
-  //   } catch (error) {
-  //     console.error('Error fetching data:', error);
-  //   }
-  // };
+  
 
   return (
     <>
     <div 
-    className='w-full h-16 bg-slate-800 text-center font-bold text-5xl text-white'>
+    className='w-full h-16 bg-gradient-to-r from-slate-600 to-blue-950 text-center font-bold text-5xl text-white'>
       News HUB
-      </div>
-      <div className='flex justify-center'>
-      <div className='grid lg:grid-cols-5 gap-10 mt-2 mr-4 sm:grid-cols-1 '>
-      {sample.map((data) => (
+      </div >
+      <div className='bg-gradient-to-r from-zinc-700 to-neutral-800 flex justify-center'>
+        
+      <div className='grid lg:grid-cols-5 gap-x-10 gap-y-5 mt-2 mr-4 sm:grid-cols-1 '>
+      
+      
+      {articles.map((data) => (
         <NewsCard 
           key={data.source.name}{...data}
-        />))};
+        />))}
         </div>
       </div>
       
--- my-app/src/component/NewsCard.tsx
+++ my-app/src/component/NewsCard.tsx
@@ -17,21 +17,26 @@ import { Card, Button } from 'react-bootstrap';
 //   }
 
   const NewsCard = (data: any ) => {
+
   return (
     <>
-      <Card  className='w-80 rounded-2xl shadow-lg hover:shadow-xl hover:shadow-cyan-500/50'>
+      <Card  className='w-80 rounded-2xl shadow-lg hover:shadow-xl border-2 bg-slate-700 border-cyan-800 hover:shadow-cyan-500/50 text-white '>
+      <a href={data.url}  target="_blank" rel="noreferrer">
       <Card.Img variant="top" src={data.urlToImage} className='w-full h-40 rounded-t-2xl border-2' />
       <Card.Body>
+      
         <Card.Title className='text-xl leading-none font-bold px-2'>{data.title}</Card.Title>
+       
         <Card.Title className='text-sm leading-none font-medium pt-2 px-2'>by {data.author}</Card.Title>
         <Card.Title className='text-xs leading-none font-thin pt-1 px-2'>{moment(data.publishedAt).format("MMM Do, YYYY")}</Card.Title>
         <Card.Text className='text-sm leading-none font-medium pt-1 px-2'>{data.source.name}</Card.Text>
 
-        <Card.Text className='text-base leading-none px-2 pt-2'>
+        <Card.Text className='text-base leading-5 px-2 py-2'>
           {data.description} 
-          <a href={data.url} className='text-blue-500 font-semibold'>See More</a>
+          <a href={data.url} className='text-blue-500 font-semibold'>..See More</a>
         </Card.Text>
       </Card.Body>
+      </a>
     </Card>
     
     </>
